a757581f32b75b9e2d637a7a79e7955dbb76bb01
Diane-SDP <diane.sautereau@gmail.com>
2026-03-30 16:28:23 +0200

    fix: added missing return (YES-30)

--- app/components/question/StandardQuestion.tsx
+++ app/components/question/StandardQuestion.tsx
@@ -2,55 +2,55 @@ import { useQuiz } from "@/app/context/quizContext";
 import { BasicQuestion, Question } from "@/app/types/quiz";
 import { router } from "expo-router";
 import { useState } from "react";
-import { Pressable,Text,View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 type Props = {
   question: BasicQuestion;
 };
 
-export default function StandardQuestion({ question }:Props){
-    const { nextQuestion,loseLife } = useQuiz();
-    const [isCorrectAnswer, setIsCorrectAnswer] = useState<boolean | null>(null);
+export default function StandardQuestion({ question }: Props) {
+  const { nextQuestion, loseLife } = useQuiz();
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState<boolean | null>(null);
 
-    function CheckAnswer(answer:string){
-        if(answer != question.answer){
-            setIsCorrectAnswer(false)
-            loseLife()
-        }
-        
-        setIsCorrectAnswer(true)
-        // RAJOUTER ANIMATION
-        nextQuestion()
-        router.push(`/quiz`)
-    }
-    
-    if (!question) {
-        return (
-        <View>
-            <Text>Question introuvable</Text>
-        </View>
-        );
+  function CheckAnswer(answer: string) {
+    if (answer != question.answer) {
+      setIsCorrectAnswer(false);
+      loseLife();
+      return;
     }
 
+    setIsCorrectAnswer(true);
+    // RAJOUTER ANIMATION
+    nextQuestion();
+    router.push(`/quiz`);
+  }
+
+  if (!question) {
     return (
-        <View>
-            <Text>{question.question}</Text>
-            <View>
-                {question.options.map((option) => (
-                    <Pressable key={option} onPress={() => CheckAnswer(option)}>
-                        <Text>{option}</Text>
-                    </Pressable>
-                ))}
-            </View>
-            <View>
-                {isCorrectAnswer !== null && (
-                isCorrectAnswer ? (
-                    <Text>Bonne réponse</Text>
-                ) : (
-                    <Text>Mauvaise réponse</Text>
-                )
-                )}
-            </View>
-        </View>
-    )
+      <View>
+        <Text>Question introuvable</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <Text>{question.question}</Text>
+      <View>
+        {question.options.map((option) => (
+          <Pressable key={option} onPress={() => CheckAnswer(option)}>
+            <Text>{option}</Text>
+          </Pressable>
+        ))}
+      </View>
+      <View>
+        {isCorrectAnswer !== null &&
+          (isCorrectAnswer ? (
+            <Text>Bonne réponse</Text>
+          ) : (
+            <Text>Mauvaise réponse</Text>
+          ))}
+      </View>
+    </View>
+  );
 }
--- app/quiz.tsx
+++ app/quiz.tsx
@@ -2,20 +2,27 @@ import { useLocalSearchParams } from "expo-router";
 import { useQuestion } from "./hooks/useQuiz";
 import StandardQuestion from "./components/question/StandardQuestion";
 import { useQuiz } from "@/app/context/quizContext";
-import { View,Text } from "react-native";
-import { Text } from "react-native";
+import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { QuizBackground } from "./components/ui/QuizBackground";
 import { quizStyles } from "../assets/style/quiz.styles";
 
 export default function QuizPage() {
-  const { chapterIndex,questionIndex } = useQuiz();
-  
-  const question = useQuestion(String(chapterIndex),questionIndex)
-  const view = <View><Text>Init</Text></View>
+  const { chapterIndex, questionIndex } = useQuiz();
 
-  if (!question){
-    view =  <View><Text>aucune question trouvé</Text></View>
+  const question = useQuestion(String(chapterIndex), questionIndex);
+  let view = (
+    <View>
+      <Text>Init</Text>
+    </View>
+  );
+
+  if (!question) {
+    view = (
+      <View>
+        <Text>aucune question trouvé</Text>
+      </View>
+    );
   }
 
   if (question.type === "basic") {
@@ -23,14 +30,11 @@ export default function QuizPage() {
   }
 
   if (question.type === "interactive") {
-    view =  <question.component/>;
+    view = <question.component />;
   }
   return (
     <QuizBackground>
-      <SafeAreaView style={quizStyles.container}>
-        {view}
-      </SafeAreaView>
+      <SafeAreaView style={quizStyles.container}>{view}</SafeAreaView>
     </QuizBackground>
   );
 }
-
