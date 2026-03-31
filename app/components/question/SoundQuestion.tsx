e4e09b9df15d2a55685d699ac1f5b27dde2a3dde
Noemyye <mimisuper48@gmail.com>
2026-03-31 14:51:56 +0200

    add front ui heart, button

--- app/components/question/SoundQuestion.tsx
+++ app/components/question/SoundQuestion.tsx
@@ -139,7 +139,6 @@ export default function SoundQuestion() {
 
   return (
     <View style={styles.container}>
-      {isWin && <Text>Bravo , bonne réponse</Text>}
       <View style={styles.answersContainer}>
         <Answers
           squared
