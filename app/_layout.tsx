57fd78e358fd8f61ef356843d88e1e3f65e204e9
Maymeric <58877868+AymericMarec@users.noreply.github.com>
2026-03-31 13:50:33 +0200

    Revert "add music"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,10 @@
 import { Stack } from "expo-router";
 import { QuizProvider } from "./context/quizContext";
-import MusicPlayer from "@/app/components/sound/MusicPlayer";
 
 export default function RootLayout() {
 
   return (
   <QuizProvider>
-    <MusicPlayer/>
     <Stack screenOptions={{ headerShown: false }} />
   </QuizProvider>
   );
